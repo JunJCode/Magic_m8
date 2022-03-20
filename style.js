5e345d34c4af3a47b63586aaa6006fb1dd1ac2a1
Billy-Night <billy.m.nightingale@gmail.com>
2022-03-20 17:41:31 +0100

    created a new variable and link it to the magicEightBall element in html, then added varibale for the textDisplay in html, used a event listener to input the result of random_answer function, set-up a variable for the triangle element in html, used an event listener to run a function that shows and hides the traingle on a click(needs adjustments to look nice)

--- style.js
+++ style.js
@@ -3,21 +3,62 @@
 let answers = ["It is certain.", "It is decidedly.", "Yes Definitely!", "You may rely on it!", "As I see it, yes!", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy.", "Try again.", "Ask again later.","Better not tell you now.","Cannot predict now.", "Concentrate and ask again.", "Don't count on it!", "My reply is no!", "My sources say no.", "Outlook not so good.", "Very doubtful."];
 
 //Declartion of a function and passing in the array of answers
-function random_items(answers){ 
 //return is used to return a value and save it to be called later
+function random_answer(){ 
     return answers[Math.floor(Math.random()*answers.length)];}
 //The code inside [] above will return a number between 0 - 20
 //The Math.floor() function returns the largest integer less than or equal to a given number.
 //The Math.random() function returns a number between 0-1
 //The Math.random() number is then * by the answers.length which is the array
+//console.log(random_answer(answers));
 
-// Below the variable has been created 
-let randomAnswer = (random_items(answers));
 
-console.log(randomAnswer);
+// Below the variable random_answer has been created to save the results of calling the function random_answer and passing in the array answers
+//let randomAnswer = (random_answer(answers));
+
+//Below both console.logs will give the same result but the first is calling a the function and passing in anwers.
+
+//console.log(random_answers(answers))
+//console.log(randomAnswer);
 
 //This will select the textDisplay ID within the HTML Document and then display the variable randomAnswer
-document.getElementById('textDisplay').innerHTML = randomAnswer;
+//document.getElementById('textDisplay').innerHTML = randomAnswer;
+
+const ballClick = document.querySelector(".magicEightBall");
+const textDisplay = document.getElementById('textDisplay');
+
+ballClick.addEventListener("click", function(){
+    textDisplay.innerHTML = random_answer(answers);
+});
+//Below will name the varable associated to the tri element in the index.html
+const ShowHidetriangle = document.getElementById('tri');
+
+//Below will set up another event listen shich will wait for an event and then run the function changing the style.css display of the html elemet tri.
+ballClick.addEventListener('click', function() {
+    //tri.style.backgroundColor="green";
+    
+    if (tri.style.display === "none") {
+        tri.style.display = "block";
+    } else {
+        tri.style.display = "none";
+    }
+});
+
+
+
+
+//This code will display the randomAnswers variable on a click
+/* ballClick.addEventListener("click", function(){
+    textDisplay.innerHTML = randomAnswer;
+}); */
+
+//The below code can input text into the p tag on the html
+/* ballClick.addEventListener("click", function(){
+    textDisplay.textContent = "This is a test";
+}); */
+
+
+
 
 /* function hideShow() {
     let x = document.getElementById("triangle");
@@ -30,12 +71,15 @@ document.getElementById('textDisplay').innerHTML = randomAnswer;
 
 //answerDisplay = random_item
 
+/* const clickEightBall = document.querySelector(".magicEightBall");
+
+clickEightBall.addEventListener("click", random_items()); */
 
 
-const eightBall = document.querySelector('.magicEightBall');
+/* const eightBall = document.querySelector('.magicEightBall');
 eightBall.addEventListener('click', function(){
      eightBall.style.backgroundColor="red";
-});
+}); */
 
 
 /* const triangle = document.getElementById('tri');
@@ -47,16 +91,6 @@ eightBall.addEventListener('click', function() {
         x.style.display = "none";
     }
 }); */
-
-const ShowHidetriangle = document.getElementById('tri');
-eightBall.addEventListener('click', function() {
-    /* tri.style.backgroundColor="green"; */
-    if (tri.style.display === "none") {
-        tri.style.display = "block";
-    } else {
-        tri.style.display = "none";
-    }
-});
 
 //
 //console.log(answers.length);
